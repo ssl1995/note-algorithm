ccb00b8f148287063b410a7060912662872fbad9
shenglinsong <shenglinsong@fintopia.tech>
2023-04-11 11:10:16 +0800

    左神笔记迁移

---
+++ note-zuo/src/main/java/com/ssl/note/zuo/NoteZuoApplication.java
@@ -0,0 +1,13 @@
+package com.ssl.note.zuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteZuoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteZuoApplication.class, args);
+    }
+
+}
---
+++ note-zuo/src/test/java/com/ssl/note/zuo/NoteZuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssl.note.zuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteZuoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
