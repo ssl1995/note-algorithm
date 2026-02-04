d8d8cdd4171a03336b359989f98c46425667be59
songshenglin <shenglinsong@fintopia.tech>
2026-02-04 11:40:01 +0800

    Top1-两数之和

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC1_两数之和/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC1_两数之和/Solution.java
@@ -10,30 +10,37 @@ import java.util.Map;
  * @description
  */
 public class Solution {
-    /**
-     * 两数之和
-     * 输入：nums = [2,7,11,15], target = 9
-     * 输出：[0,1]
-     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
-     */
-    public int[] twoSum(int[] nums, int target) {
-        if (nums.length < 1) {
-            return new int[]{-1, -1};
-        }
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{i, map.get(target - nums[i])};
-            }
-            map.put(nums[i], i);
-        }
-        return new int[]{-1, -1};
-    }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+  /**
+   * 两数之和
+   * 输入：nums = [2,7,11,15], target = 9
+   * 输出：[0,1]
+   * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+   */
+  public int[] twoSum(int[] nums, int target) {
+    if (nums == null || nums.length <= 1) {
+      return new int[]{-1, -1};
     }
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i <= nums.length - 1; i++) {
+      int otherNum = target - nums[i];
+      // 单词：containsKey
+      if (!map.containsKey(otherNum)) {
+        map.put(nums[i], i);
+        // 单词：continue
+        continue;
+      }
+      int otherIndex = map.get(otherNum);
+      return new int[]{otherIndex, i};
+    }
+    return new int[]{-1, -1};
+
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+  }
 }
