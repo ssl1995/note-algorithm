5f3055536c45902b34d1230d5a7c960a7fdf6a2a
songshenglin <13639120050@163.com>
2026-02-06 23:26:31 +0800

    LC15_三数之和

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC15_三数之和/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC15_三数之和/Solution1.java
@@ -6,43 +6,60 @@ import java.util.List;
 
 public class Solution1 {
 
+  /**
+   * 三数之和
+   * 是否存在3个数使得a+b+c=0
+   * 输入：nums = [-1,0,1,2,-1,-4]
+   * 输出：[[-1,-1,2],[-1,0,1]]
+   */
   public List<List<Integer>> threeSum(int[] nums) {
-    if (nums == null || nums.length == 0) {
+    if (nums == null || nums.length < 3) {
       return new ArrayList<>();
     }
     List<List<Integer>> res = new ArrayList<>();
+    // 1.排序
     Arrays.sort(nums);
-
-
-    for (int k = 0; k < nums.length - 2; k++) {
-      if (nums[k] > 0) {
+    int n = nums.length;
+    for (int i = 0; i < n - 2; i++) {
+      // 2.剪枝：最小值大于0，不可能有解
+      if (nums[i] > 0) {
+        break;
+      }
+      // 3.去重：跳过重复的第一个数
+      if (i > 0 && nums[i] == nums[i - 1]) {
         continue;
       }
-      if (k > 0 && nums[k] == nums[k - 1]) {
-        continue;
-      }
-      int i = k + 1;
-      int j = nums.length - 1;
-      while (i < j) {
-        int sum = nums[k] + nums[i] + nums[j];
+      // 4.双指针
+      int left = i + 1;
+      int right = n - 1;
+      while (left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
         if (sum < 0) {
-          i++;
+          left++;
         } else if (sum > 0) {
-          j--;
+          right--;
         } else {
-          res.add(Arrays.asList(nums[k], nums[i], nums[j]));
-          i++;
-          j--;
-          while (i < j && nums[i] == nums[i - 1]) {
-            i++;
+          // 5.找到答案
+          res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+          // 6.跳过重复值
+          while (left < right && nums[left] == nums[left + 1]) {
+            left++;
           }
-          while (i < j && nums[j] == nums[j + 1]) {
-            j--;
+          while (left < right && nums[right] == nums[right - 1]) {
+            right--;
           }
+          left++;
+          right--;
         }
       }
     }
-
     return res;
   }
+
+  public static void main(String[] args) {
+    Solution1 solution = new Solution1();
+    int[] nums = {-1, 0, 1, 2, -1, -4};
+    System.out.println(solution.threeSum(nums));
+  }
+
 }
