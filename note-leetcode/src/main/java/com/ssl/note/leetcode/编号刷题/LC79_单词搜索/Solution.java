afde7b60008122210b658c7ccf30e365a881e093
songshenglin <13639120050@163.com>
2026-02-13 10:16:22 +0800

    LC79_单词搜索

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC79_单词搜索/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC79_单词搜索/Solution.java
@@ -7,49 +7,56 @@ package com.ssl.note.leetcode.编号刷题.LC79_单词搜索;
  */
 public class Solution {
 
-    /**
-     * 单词搜索
-     * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
-     * 输出：true
-     */
-    public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                // 从任意一点出发，该点为起始点，匹配成功一次就算成功
-                if (dfs(board, i, j, word, 0)) {
-                    return true;
-                }
-            }
+  /**
+   * 单词搜索
+   * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+   * 输出：true
+   */
+  public boolean exist(char[][] board, String word) {
+    int m = board.length;
+    int n = board[0].length;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (backtrack(board, i, j, word, 0)) {
+          return true;
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    private boolean dfs(char[][] board, int i, int j, String word, int index) {
-        if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1 || board[i][j] != word.charAt(index)) {
-            return false;
-        }
-        if (index == word.length() - 1) {
-            return true;
-        }
-        // 剪枝：搜索过的单词，要特殊标记后，防止后面遍历重复访问
-        board[i][j] = '*';
-        // 从(i,j)位置上下左右都递归一遍，只要有一个匹配就成功
-        boolean res = dfs(board, i + 1, j, word, index + 1)
-                || dfs(board, i - 1, j, word, index + 1)
-                || dfs(board, i, j + 1, word, index + 1)
-                || dfs(board, i, j - 1, word, index + 1);
-
-        // 还原：访问判断过后的单词
-        board[i][j] = word.charAt(index);
-        return res;
+  /*
+   * 回溯= dfs+撤销
+   */
+  private boolean backtrack(char[][] board, int i, int j, String word, int index) {
+    // 结束条件1：失败
+    if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1
+        || board[i][j] != word.charAt(index)) {
+      return false;
     }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
-        String word = "ABCCED";
-        System.out.println(solution.exist(board, word));
+    // 结束条件2：word全部匹配成功
+    if (index == word.length() - 1) {
+      return true;
     }
+    // 做选择：原地标记
+    // 防止重复：向四个方向递归，第一次是4个方向，后面都是3个方向，因为标记的来时路不能重复
+    board[i][j] = '*';
+    // 四个方向找
+    boolean found = backtrack(board, i - 1, j, word, index + 1)
+        || backtrack(board, i + 1, j, word, index + 1)
+        || backtrack(board, i, j - 1, word, index + 1)
+        || backtrack(board, i, j + 1, word, index + 1);
+    // 回溯特征撤销选择：标记还原，递归结束，返回上一层
+    board[i][j] = word.charAt(index);
+    return found;
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+    String word = "ABCCED";
+    System.out.println(solution.exist(board, word));
+  }
 
 
 }
