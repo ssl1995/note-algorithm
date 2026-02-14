ba06148bfe806dbe92c6a850a3c18c8d0d39fdf7
songshenglin <13639120050@163.com>
2026-02-14 08:13:01 +0800

    LC141_环形链表I

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC141_环形链表I/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC141_环形链表I/Solution.java
@@ -3,24 +3,25 @@ package com.ssl.note.leetcode.编号刷题.LC141_环形链表I;
 import com.ssl.note.leetcode.utils.ListNode;
 
 public class Solution {
-    /**
-     * 判断链表是否有环，返回值是布尔值
-     */
-    public boolean hasCycle(ListNode head) {
-        if (head == null || head.next == null || head.next.next == null) {
-            return false;
-        }
-        // 初始化：快指针在next.next,慢指针在next上
-        ListNode slow = head.next;
-        ListNode fast = head.next.next;
-        while (fast != slow) {
-            if (fast == null || fast.next == null || fast.next.next == null) {
-                return false;
-            }
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return true;
+
+  /**
+   * 判断链表是否有环，返回值是布尔值
+   */
+  public boolean hasCycle(ListNode head) {
+    if (head == null || head.next == null) {
+      return false;
     }
+    ListNode slow = head;
+    ListNode fast = head;
+    // 快慢指针
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
