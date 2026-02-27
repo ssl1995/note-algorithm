e05a7bb5faf43fc6d6c679400066c6fc53c4febe
songshenglin <13639120050@163.com>
2026-02-28 01:15:44 +0800

    LC148_排序链表

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC148_排序链表/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC148_排序链表/Solution.java
@@ -3,64 +3,67 @@ package com.ssl.note.leetcode.编号刷题.LC148_排序链表;
 import com.ssl.note.leetcode.utils.ListNode;
 
 public class Solution {
-    /**
-     * 排序链表
-     * 使用归并排序，升序排序
-     */
-    public ListNode sortList(ListNode head) {
-        return mergeSortList(head);
+
+  /**
+   * 排序链表
+   * 输入：head = [4,2,1,3]
+   * 输出：[1,2,3,4]
+   * 方法：归并排序法
+   */
+  public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    // 拆
+    ListNode mid = getMiddleNode(head);
+    ListNode rightHead = mid.next;
+    mid.next = null;
+
+    // 排
+    ListNode left = sortList(head);
+    ListNode right = sortList(rightHead);
+
+    // 合
+    return merge(left, right);
+  }
+
+
+  // 偶数取左边
+  private ListNode getMiddleNode(ListNode head) {
+    if (head == null) {
+      return null;
+    }
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+
+  private ListNode merge(ListNode left, ListNode right) {
+    if (left == null || right == null) {
+      return left == null ? right : left;
     }
 
-    private ListNode mergeSortList(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        // 找到中点位置：链表奇数长返回中点，偶数长返回中间靠左位置
-        ListNode middle = getMidNode(head);
-        // 记录中点下一个位置
-        ListNode next = middle.next;
-        // 切割中点，一定要中间.next置空
-        middle.next = null;
+    ListNode dummy = new ListNode(-1);
+    ListNode cur = dummy;
 
-        // 以左边的头head开始递归
-        ListNode left = mergeSortList(head);
-        // 以右边的头next开始递归
-        ListNode right = mergeSortList(next);
-
-        // 合并两个有序链表
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy;
-        while (left != null & right != null) {
-            if (left.val <= right.val) {
-                cur.next = left;
-                left = left.next;
-            } else {
-                cur.next = right;
-                right = right.next;
-            }
-            cur = cur.next;
-        }
-        cur.next = left != null ? left : right;
-        return dummy.next;
+    while (left != null && right != null) {
+      if (left.val < right.val) {
+        cur.next = left;
+        left = left.next;
+      } else {
+        cur.next = right;
+        right = right.next;
+      }
+      cur = cur.next;
     }
 
-    /**
-     * 找到链表中心：奇数是中点，偶数是中心靠左的位置
-     */
-    private ListNode getMidNode(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode fast = head.next;
-        ListNode slow = head;
+    cur.next = left == null ? right : left;
 
-        // 画图，slow此时就指向奇数中点/偶数靠左的位置
-        while (fast != null && fast.next != null) {
-            // fast走两步
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
-    }
+    return dummy.next;
+  }
 
 }
