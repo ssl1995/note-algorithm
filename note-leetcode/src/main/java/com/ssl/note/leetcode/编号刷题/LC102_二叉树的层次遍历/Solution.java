ac6314266ed4d6548e148707ee67f6b32f114ea4
songshenglin <shenglinsong@fintopia.tech>
2026-03-02 10:08:35 +0800

    LC102_二叉树的层次遍历

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC102_二叉树的层次遍历/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC102_二叉树的层次遍历/Solution.java
@@ -2,42 +2,45 @@ package com.ssl.note.leetcode.编号刷题.LC102_二叉树的层次遍历;
 
 import com.ssl.note.leetcode.utils.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
-    /**
-     * 二叉树的层次遍历
-     */
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
-        }
-        LinkedList<TreeNode> queue = new LinkedList<>();
-        // 根节点单独的一层
-        queue.offer(root);
-
-        List<List<Integer>> res = new ArrayList<>();
-
-        while (!queue.isEmpty()) {
-            List<Integer> temp = new ArrayList<>();
-            // 难点：初始时，当前层的元素个数不能变，所以从size开始--
-            for (int i = queue.size(); i > 0; i--) {
-
-                TreeNode node = queue.poll();
-                temp.add(node.val);
-
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
-            }
-
-            res.add(temp);
-        }
-        return res;
+  /**
+   * 二叉树的层次遍历
+   */
+  public List<List<Integer>> levelOrder(TreeNode root) {
+    if (root == null) {
+      return new ArrayList<>();
     }
+    List<List<Integer>> res = new ArrayList<>();
+
+    Deque<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      // 关键：size锁定每一层从队列中遍历的长度
+      int size = queue.size();
+      // 每一层保存的遍历数据
+      List<Integer> level = new ArrayList<>();
+
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        if (node != null) {
+          level.add(node.val);
+          if (node.left != null) {
+            queue.offer(node.left);
+          }
+          if (node.right != null) {
+            queue.offer(node.right);
+          }
+        }
+      }
+
+      res.add(level);
+    }
+
+    return res;
+  }
+
+
 }
