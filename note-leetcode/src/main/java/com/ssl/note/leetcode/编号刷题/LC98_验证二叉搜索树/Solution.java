9869456782aa8de0fdb3833802ee7af490a10120
songshenglin <shenglinsong@fintopia.tech>
2026-03-03 09:27:21 +0800

    LC98_验证二叉搜索树

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC98_验证二叉搜索树/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC98_验证二叉搜索树/Solution.java
@@ -7,68 +7,72 @@ import java.util.LinkedList;
 
 public class Solution {
 
-    /**
-     * pre是会超过int类型的边界值,所以使用Long的边界值
-     */
-    private long pre = Long.MIN_VALUE;
+  /**
+   * pre是会超过int类型的边界值,所以使用Long的边界值
+   */
+  private long pre = Long.MIN_VALUE;
 
-    /**
-     * 验证是否是BST
-     * 中序递归法
-     */
-    public boolean isValidBST(TreeNode root) {
-        // 空节点，是BST，返回true
-        if (root == null) {
-            return true;
-        }
-        // 中序遍历
-        // 左孩子是否是BST
-        boolean leftIsBST = isValidBST(root.left);
-        if (!leftIsBST) {
-            return false;
-        }
+  /**
+   * 验证是否是BST
+   * 中序递归法
+   */
+  public boolean isValidBST(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    // 左子树
+    boolean left = isValidBST(root.left);
+    if (!left) {
+      return false;
+    }
 
-        // BST的中序遍历：小到大
+    // 陷阱：不能只比较当前节点: 左<根<右
+    // 正确：子树所有节点<根<右子树所有节点，而不只是直接孩子
+    if (root.val <= pre) {
+      return false;
+    }
+    pre = root.val;
+
+    // 右子树
+    boolean right = isValidBST(root.right);
+    if (!right) {
+      return false;
+    }
+
+    return true;
+  }
+
+
+  /**
+   * 验证是否是BST
+   * 中序非递归
+   */
+  public boolean isValidBST1(TreeNode root) {
+    if (root == null) {
+      return true;
+    }
+    LinkedList<TreeNode> stack = new LinkedList<>();
+    long pre = Long.MIN_VALUE;
+    while (!stack.isEmpty() || root != null) {
+      // 中序非递归遍历先把所有左子树入栈
+      if (root != null) {
+        stack.push(root);
+        root = root.left;
+      } else {
+        // 左子树到null就出栈，操作+入右子树
+        root = stack.pop();
+
         if (root.val <= pre) {
-            return false;
+          return false;
+        } else {
+          pre = root.val;
         }
-        pre = root.val;
 
-        // 右孩子是否是BST
-        return isValidBST(root.right);
+        root = root.right;
+      }
     }
-
-
-    /**
-     * 验证是否是BST
-     * 中序非递归
-     */
-    public boolean isValidBST1(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        LinkedList<TreeNode> stack = new LinkedList<>();
-        long pre = Long.MIN_VALUE;
-        while (!stack.isEmpty() || root != null) {
-            // 中序非递归遍历先把所有左子树入栈
-            if (root != null) {
-                stack.push(root);
-                root = root.left;
-            } else {
-                // 左子树到null就出栈，操作+入右子树
-                root = stack.pop();
-
-                if (root.val <= pre) {
-                    return false;
-                } else {
-                    pre = root.val;
-                }
-
-                root = root.right;
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 
 
 }
--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC98_验证二叉搜索树/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC98_验证二叉搜索树/Solution1.java
@@ -2,41 +2,46 @@ package com.ssl.note.leetcode.编号刷题.LC98_验证二叉搜索树;
 
 import com.ssl.note.leetcode.utils.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class Solution1 {
 
-    // Q:利用Morris方法判断是否是搜索二叉树
-    public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        TreeNode cur = root;
-        TreeNode mostRight = null;
-        // 力扣边界:这里使用int会越界,所以使用Long的最小值
-        long preValue = Long.MIN_VALUE;
-        while (cur != null) {
-            mostRight = cur.left;
-            if (mostRight != null) {
-                while (mostRight.right != null && mostRight.right != cur) {
-                    mostRight = mostRight.right;
-                }
-                if (mostRight.right == null) {
-                    mostRight.right = cur;
-                    cur = cur.left;
-                    continue;
-                } else {
-                    mostRight.right = null;
-                }
-            }
-            // 如果第二次来到的结点值不产生升序,就不是二叉搜索树
-            if (cur.val <= preValue) {
-                return false;
-            }
-            // 否则,更新pre
-            preValue = cur.val;
-
-            cur = cur.right;
-        }
-        return true;
+  /**
+   * 验证是否是BST
+   * 中序非递归
+   */
+  public boolean isValidBST(TreeNode root) {
+    if (root == null) {
+      return true;
     }
+    // 中序遍历是用栈
+    Deque<TreeNode> stack = new ArrayDeque<>();
+    long pre = Long.MIN_VALUE;
+
+    while (!stack.isEmpty() || root != null) {
+      if (root != null) {
+        stack.push(root);
+        root = root.left;
+      } else {
+        TreeNode pop = stack.pop();
+
+        // 陷阱：不能只比较当前节点: 左<根<右
+        // 正确：子树所有节点<根<右子树所有节点，而不只是直接孩子
+        if (pop.val <= pre) {
+          return false;
+        }
+        pre = pop.val;
+        // 中序非递归写法只有遍历左子树才push，这里只是切换遍历指针
+        root = pop.right;
+      }
+    }
+
+    return true;
+  }
+
+
 }
