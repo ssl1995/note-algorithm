b8a4ea02c820ab4471397dc5636ea6f4a21383e4
songshenglin <13639120050@163.com>
2026-03-08 13:16:47 +0800

    LC208_实现前缀树-1

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC208_实现前缀树/Trie.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC208_实现前缀树/Trie.java
@@ -1,62 +1,66 @@
 package com.ssl.note.leetcode.编号刷题.LC208_实现前缀树;
 
-/**
- * @author SongShengLin
- * @date 2022/6/18 18:25
- * @description
- */
 public class Trie {
 
-    static class TrieNode {
-        public TrieNode[] children;
+  // 前缀树一定要有根
+  TrieNode root;
 
-        public boolean isWord;
+  public Trie() {
+    root = new TrieNode();
+  }
 
-        public TrieNode() {
-            // 只有小写字母
-            children = new TrieNode[26];
-        }
+  // 前缀树加上一个单词
+  public void insert(String word) {
+    TrieNode cur = root;
+
+    for (char c : word.toCharArray()) {
+      int index = c - 'a';
+      if (cur.children[index] == null) {
+        TrieNode child = new TrieNode();
+        cur.children[index] = child;
+      }
+      // 找下一个
+      cur = cur.children[index];
+    }
+    // 插入操作，末尾节点标记为true
+    cur.isEnd = true;
+  }
+
+  // 查询单词=isEnd必须为true
+  public boolean search(String word) {
+    return searchWord(word, false);
+  }
+
+  // 前缀查询=isEnd不需要为true，只要存在就行
+  public boolean startsWith(String prefix) {
+    return searchWord(prefix, true);
+  }
+
+  // 通用的查询方法，搜索单词或者前缀
+  private boolean searchWord(String word, boolean isSearchPrefix) {
+    TrieNode cur = root;
+
+    for (char c : word.toCharArray()) {
+      int index = c - 'a';
+      if (cur.children[index] == null) {
+        return false;
+      }
+      cur = cur.children[index];
     }
 
-    private TrieNode root;
+    return isSearchPrefix || cur.isEnd;
+  }
 
-    public Trie() {
-        root = new TrieNode();
+
+  static class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+
+    public TrieNode() {
+      // 初始化数组，长度为26
+      // 只适用于26个字母
+      children = new TrieNode[26];
     }
+  }
 
-    public void insert(String word) {
-        TrieNode cur = root;
-        for (char c : word.toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
-                TrieNode node = new TrieNode();
-                cur.children[c - 'a'] = node;
-            }
-            cur = cur.children[c - 'a'];
-        }
-        cur.isWord = true;
-    }
-
-    public boolean search(String word) {
-        TrieNode cur = root;
-        for (char c : word.toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
-                return false;
-            }
-            cur = cur.children[c - 'a'];
-        }
-
-        return cur.isWord;
-    }
-
-    public boolean startsWith(String prefix) {
-        TrieNode cur = root;
-        for (char c : prefix.toCharArray()) {
-            if (cur.children[c - 'a'] == null) {
-                return false;
-            }
-            cur = cur.children[c - 'a'];
-        }
-
-        return true;
-    }
 }
