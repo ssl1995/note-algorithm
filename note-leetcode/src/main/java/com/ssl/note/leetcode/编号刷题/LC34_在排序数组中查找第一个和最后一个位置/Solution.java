681360f5db64f1aba8c5b1b544948a90942368b5
songshenglin <13639120050@163.com>
2026-03-14 09:28:48 +0800

    LC34_在排序数组中查找第一个和最后一个位置-1

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC34_在排序数组中查找第一个和最后一个位置/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC34_在排序数组中查找第一个和最后一个位置/Solution.java
@@ -1,52 +1,76 @@
 package com.ssl.note.leetcode.编号刷题.LC34_在排序数组中查找第一个和最后一个位置;
 
+import java.util.Arrays;
+
 /**
  * @author SongShengLin
- * @date 2021/12/26 6:15 PM
+ * @date 2022/1/15 12:10 PM
  * @description
  */
 public class Solution {
 
-    /**
-     * 在排序数组中查找第一个和最后一个位置
-     * 输入：nums = [5,7,7,8,8,10], target = 8
-     * 输出：[3,4]
-     */
-    public int[] searchRange(int[] nums, int target) {
-        if (!isContainTarget(nums, target)) {
-            return new int[]{-1, -1};
-        }
-        // 获取大于t-1的第一个数的下标 = t的第一个数的下标
-        // 获取大于t的第一个数的下标-1 = t的最后一个数的下标
-        return new int[]{getNextTargetFirstIndex(nums, target - 1), getNextTargetFirstIndex(nums, target) - 1};
+  /**
+   * 在排序数组中查找第一个和最后一个位置
+   * 输入：nums = [5,7,7,8,8,10], target = 8
+   * 输出：[3,4]
+   */
+  public int[] searchRange(int[] nums, int target) {
+    if (nums.length == 0) {
+      return new int[]{-1, -1};
     }
+    return new int[]{binarySort1(nums, target), binarySort2(nums, target)};
+  }
 
-    private boolean isContainTarget(int[] nums, int target) {
-        for (int num : nums) {
-            if (num == target) {
-                return true;
-            }
+  /**
+   * 返回>=t的第一个下标
+   */
+  private int binarySort1(int[] nums, int t) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < t) {
+        left++;
+      } else if (nums[mid] > t) {
+        right--;
+      } else {
+        if (mid == 0 || nums[mid - 1] < t) {
+          return mid;
         }
-        return false;
+        // 找第一个
+        right--;
+      }
     }
+    return -1;
+  }
 
-    /**
-     * 获取>target的第一个数的下标
-     */
-    private int getNextTargetFirstIndex(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            // 经验:二分法中<=就是返回t的右边界
-            if (arr[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+  /**
+   * 返回<=t的第一个下标
+   */
+  private int binarySort2(int[] nums, int t) {
+    int left = 0;
+    int right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] < t) {
+        left++;
+      } else if (nums[mid] > t) {
+        right--;
+      } else {
+        if (mid == nums.length - 1 || nums[mid + 1] > t) {
+          return mid;
         }
-        // left <= right打破的时候，left来到>target的第一个数的下标
-        return left;
+        // 找最后一个
+        left++;
+      }
     }
+    return -1;
+  }
 
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] nums = {1, 1, 2};
+    int t = 1;
+    System.out.println(Arrays.toString(solution.searchRange(nums, t)));
+  }
 }
