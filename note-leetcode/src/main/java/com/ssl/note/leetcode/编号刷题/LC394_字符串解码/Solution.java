9db52ecb6c68f34afcbe9c1402bbca6d474ac3b5
songshenglin <13639120050@163.com>
2026-03-15 12:23:12 +0800

    LC394_字符串解码

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC394_字符串解码/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC394_字符串解码/Solution.java
@@ -1,70 +1,62 @@
 package com.ssl.note.leetcode.编号刷题.LC394_字符串解码;
 
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class Solution {
 
-    /**
-     * 字符串编码
-     * 输入：s = "3[a]2[bc]"
-     * 输出："aaabcbc"
-     * 输入：s = "3[a2[c]]"
-     * 输出："accaccacc"
-     */
-    public String decodeString(String s) {
-        if (s == null) {
-            return "";
-        }
-        // 栈1：保存遇到的倍数数字
-        Deque<Integer> numStack = new LinkedList<>();
-        // 栈2：保存已经生成好的字符串
-        Deque<String> letterStack = new LinkedList<>();
-
-        // num保存遇到左括号前的数字=倍数
-        int multi = 0;
-        // res在本方法返回之作为保存[]之间，临时字符的功能
-        StringBuilder res = new StringBuilder();
-
-        // s = "3[a2[c]]"
-        for (char c : s.toCharArray()) {
-            if (c >= '0' && c <= '9') {
-                // 遇到数字
-                // num记录数字值
-                multi = multi * 10 + (c - '0');
-            } else if (c == '[') {
-                // 遇到左括号
-
-                // 记录之前的倍数num=multi
-                numStack.push(multi);
-                // 记录之前生成的字符串结果
-                letterStack.push(res.toString());
-
-                multi = 0;
-                res = new StringBuilder();
-            } else if (c == ']') {
-                // 遇到右括号
-                StringBuilder temp = new StringBuilder();
-                int lastNum = numStack.pop();
-                for (int i = 0; i < lastNum; i++) {
-                    temp.append(res);
-                }
-
-                res = new StringBuilder(letterStack.pop() + temp);
-            } else {
-                // 遇到字符
-                res.append(c);
-            }
-        }
-
-        return res.toString();
-
+  /**
+   * 字符串编码
+   * 输入：s = "3[a]2[bc]"
+   * 输出："aaabcbc"
+   * 输入：s = "3[a2[c]]"
+   * 输出："accaccacc"
+   */
+  public String decodeString(String s) {
+    if (s == null) {
+      return "";
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String s = "3[a2[c]]";
-        // accaccacc
-        System.out.println(solution.decodeString(s));
+    StringBuilder res = new StringBuilder();
+
+    Deque<Integer> numsStack = new ArrayDeque<>();
+    Deque<String> letterStack = new ArrayDeque<>();
+    int multi = 0;
+
+    char[] cs = s.toCharArray();
+    for (char c : cs) {
+      // 数字
+      if (c >= '0' && c <= '9') {
+        multi = multi * 10 + (c - '0');
+      } else if (c == '[') {
+        // 左括号，压入数字栈和字母栈
+        numsStack.push(multi);
+        letterStack.push(res.toString());
+        multi = 0;
+        res = new StringBuilder();
+      } else if (c == ']') {
+        // 右括号，弹出数字栈和字母栈，拼接字符串
+        StringBuilder temp = new StringBuilder();
+        int count = numsStack.pop();
+        for (int i = 0; i < count; i++) {
+          temp.append(res);
+        }
+        // 与弹出的字母栈拼接
+        res = new StringBuilder(letterStack.pop() + temp);
+      } else {
+        // 字母，保存当前res
+        res.append(c);
+      }
     }
+
+    return res.toString();
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    String s = "3[a2[c]]";
+    // accaccacc
+    System.out.println(solution.decodeString(s));
+  }
 }
