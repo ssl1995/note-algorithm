35089930fe7693dd6795f37b7f639312e45feaf9
songshenglin <13639120050@163.com>
2026-03-16 23:50:38 +0800

    LC347_前K个高频元素

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC347_前K个高频元素/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC347_前K个高频元素/Solution1.java
@@ -3,7 +3,7 @@ package com.ssl.note.leetcode.编号刷题.LC347_前K个高频元素;
 import java.util.*;
 
 
-public class Solution {
+public class Solution1 {
 
   /**
    * 前k个高频元素
@@ -11,20 +11,18 @@ public class Solution {
    * 输出: [1,2]
    */
   public int[] topKFrequent(int[] nums, int k) {
-    // map存数字和其出现的频率
     Map<Integer, Integer> map = new HashMap<>();
     for (int num : nums) {
       map.put(num, map.getOrDefault(num, 0) + 1);
     }
 
-    // 最小值堆存前k大的频率的Entry:按照value从小到大排序，因为要把教小的排出去
+//    PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
     PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
 
     for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
       if (minHeap.size() < k) {
         minHeap.offer(entry);
       } else if (minHeap.peek() != null && minHeap.peek().getValue() < entry.getValue()) {
-        // 最小值堆堆顶是小的，出堆
         minHeap.poll();
         minHeap.offer(entry);
       }
@@ -36,11 +34,12 @@ public class Solution {
         res[i] = minHeap.poll().getKey();
       }
     }
+
     return res;
   }
 
   public static void main(String[] args) {
-    Solution solution1 = new Solution();
+    Solution1 solution1 = new Solution1();
     int[] nums = {1, 1, 1, 2, 2, 3};
     int t = 2;
     System.out.println(Arrays.toString(solution1.topKFrequent(nums, t)));
