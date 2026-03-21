8f67487959cbe1dec7c1d206a23a1f8e0e7b0760
songshenglin <13639120050@163.com>
2026-03-22 00:20:21 +0800

    LC300_最长递增子序列

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC300_最长递增子序列/Solution.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC300_最长递增子序列/Solution.java
@@ -4,39 +4,43 @@ import java.util.Arrays;
 
 public class Solution {
 
-    /**
-     * 最长递增子序列
-     * 输入：nums = [10,9,2,5,3,7,101,18]
-     * 输出：4
-     * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4
-     */
-    public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        // dp[i]：[0..i]组成的最长子序列长度，初始化为1
-        int[] dp = new int[nums.length];
-        // 初试化：最长连续子序列长度最低为1
-        Arrays.fill(dp, 1);
+  /**
+   * 最长递增子序列
+   * 输入：nums = [10,9,2,5,3,7,101,18]
+   * 输出：4
+   * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4
+   * 时间复杂度：O(n^2)，稍微有点长，可以用下一个版本的优化
+   * 空间复杂度：O(n)
+   */
+  public int lengthOfLIS(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int n = nums.length;
+    // dp[i]表示以nums[i]为结尾的最长递增子序列的长度
+    int[] dp = new int[n];
+    // 每一个数都可以是自己的子序列
+    Arrays.fill(dp, 1);
 
-        int manLen = 1;
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (nums[j] < nums[i]) {
-                    // 动态规划
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+    int maxLen = 1;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i; j++) {
+        // 子序列，递增
+        if (nums[j] < nums[i]) {
+          dp[i] = Math.max(dp[i], dp[j] + 1);
 
-                    manLen = Math.max(dp[i], manLen);
-                }
-            }
+          maxLen = Math.max(maxLen, dp[i]);
         }
-        return manLen;
+      }
     }
 
+    return maxLen;
+  }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] arr1 = {10, 9, 1, 5, 2, 6, 66, 18};
-        System.out.println(solution.lengthOfLIS(arr1));
-    }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] arr1 = {10, 9, 1, 5, 2, 6, 66, 18};
+    System.out.println(solution.lengthOfLIS(arr1));
+  }
 }
