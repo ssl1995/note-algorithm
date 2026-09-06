98f02e559a62e915b459c5de1aaaaa15452c740f
songshenglin <872912679@qq.com>
2026-09-06 18:32:41 +0800

    矩阵2

--- note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC54_螺旋矩阵/Solution1.java
+++ note-leetcode/src/main/java/com/ssl/note/leetcode/编号刷题/LC54_螺旋矩阵/Solution1.java
@@ -3,7 +3,7 @@ package com.ssl.note.leetcode.编号刷题.LC54_螺旋矩阵;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+public class Solution1 {
 
   /**
    * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
@@ -26,28 +26,43 @@ public class Solution {
     return res;
   }
 
-  // 闭区间取值
+  // 开区间取值
   private void f(int[][] matrix, int a, int b, int c, int d) {
-    for (int j = b; j <= d; j++) {
-      res.add(matrix[a][j]);
-    }
-    for (int i = a + 1; i <= c; i++) {
-      res.add(matrix[i][d]);
-    }
-    if (a < c) {
-      for (int j = d - 1; j >= b; j--) {
-        res.add(matrix[c][j]);
+    // 只剩一行：闭区间直接填完，结束
+    if (a == c) {
+      for (int i = b; i <= d; i++) {
+        res.add(matrix[a][i]);
       }
+      return;
     }
-    if (b < d) {
-      for (int i = c - 1; i > a; i--) {
+    // 只剩一列：闭区间直接填完，结束
+    if (b == d) {
+      for (int i = a; i <= c; i++) {
         res.add(matrix[i][b]);
       }
+      return;
+    }
+
+    // 以下保证至少两行两列，四条边开区间各留一个角给下一条边
+    for (int i = b; i < d; i++) {
+      res.add(matrix[a][i]);
+    }
+
+    for (int i = a; i < c; i++) {
+      res.add(matrix[i][d]);
+    }
+
+    for (int i = d; i > b; i--) {
+      res.add(matrix[c][i]);
+    }
+
+    for (int i = c; i > a; i--) {
+      res.add(matrix[i][b]);
     }
   }
 
   public static void main(String[] args) {
-    Solution solution = new Solution();
+    Solution1 solution = new Solution1();
     int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
 //    int[][] matrix = {{1}, {2}, {3}};
     System.out.println(solution.spiralOrder(matrix));
